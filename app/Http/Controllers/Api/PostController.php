ab43e9feaeb25881bb27a7555ce1e808b43256e4
Md Enzamamul Haque Shiplu <shiplu2111@gmail.com>
2025-04-19 01:36:47 +0600

    blog api added

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Firefly\FilamentBlog\Facades\SEOMeta;
+use Firefly\FilamentBlog\Models\NewsLetter;
+use Firefly\FilamentBlog\Models\Post;
+use Firefly\FilamentBlog\Models\ShareSnippet;
+use Firefly\FilamentBlog\Models\Category;
+use Firefly\FilamentBlog\Models\Tag;
+class PostController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        SEOMeta::setTitle('Blog | '.config('app.name')) ;
+
+        $posts = Post::query()->with(['categories', 'user', 'tags'])
+            ->published()
+            ->paginate(10);
+            foreach ($posts->items() as $post) {
+                $post->cover_photo_path = asset('storage/' . ltrim($post->cover_photo_path, '/'));
+            }
+         return response()->json(
+             [
+                 'success' => true,
+                 'message' => 'Posts fetched successfully',
+                 'data' => $posts
+             ]
+         );
+    }
+
+
+    public function search(Request $request)
+    {
+        SEOMeta::setTitle('Search result for '.$request->get('query'));
+
+        $request->validate([
+            'query' => 'required',
+        ]);
+        $posts = Post::query()
+            ->with(['categories', 'user'])
+            ->published()
+            ->whereAny(['title', 'sub_title'], 'like', '%'.$request->get('query').'%')
+            ->paginate(10)->withQueryString();
+
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Posts fetched successfully',
+                'data' => $posts
+            ]
+        );
+
+    }
+
+
+    public function show(Post $post)
+    {
+
+        $seoMeta = $post->seoDetail?->title;
+
+        $seoDescription = $post->seoDetail?->description;
+        $seoKeywords = $post->seoDetail->keywords ?? [];
+
+        $shareButton = ShareSnippet::query()->active()->first();
+        $post->load(['user', 'categories', 'tags', 'comments' => fn ($query) => $query->approved(), 'comments.user']);
+
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Post fetched successfully',
+                'data' => $post,
+                'seoData' => [
+                    'title' => $seoMeta,
+                    'description' => $seoDescription,
+                    'keywords' => $seoKeywords,
+                ],
+
+            ]
+        );
+    }
+
+    public function subscribe(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|unique:fblog_news_letters,email',
+        ], [
+            'email.unique' => 'You have already subscribed',
+        ]);
+        NewsLetter::create([
+            'email' => $request->email,
+        ]);
+
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'You have successfully subscribed to our news letter',
+            ]
+            );
+
+    }
+
+    public function tagPost(Tag $tag)
+    {
+        $posts = $tag->load(['posts.user'])
+            ->posts()
+            ->published()
+            ->paginate(25);
+
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Posts fetched successfully',
+                'data' => $posts,
+            ]
+        );
+    }
+
+    public function categoryPost(Request $request, Category $category)
+    {
+        $posts = $category->load(['posts.user', 'posts.categories'])
+            ->posts()
+            ->published()
+            ->paginate(25);
+
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Posts fetched successfully',
+                'data' => $posts,
+            ]
+            );
+    }
+    public function comment(Request $request, Post $post)
+    {
+        $request->validate([
+            'comment' => 'required|min:3|max:500',
+        ]);
+
+        $post->comments()->create([
+            'comment' => $request->comment,
+            // 'user_id' => $request->user()->id,
+            'approved' => false,
+        ]);
+
+        return response()->json(
+         [
+             'success' => true,
+             'message' => 'Thank you for your comment',
+             'comment_status' => 'Comment submitted for approval.',
+         ]
+        );
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\HeroController;
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/heros', [HeroController::class, 'index']);
 Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/blogs', [PostController::class, 'index']);
+Route::get('/blog/search', [PostController::class, 'search']);
+Route::get('/blog/{post:slug}', [PostController::class, 'show']);
+Route::post('/subscribe', [PostController::class, 'subscribe'])
+            ->middleware('throttle:5,1')
+            ->name('filamentblog.post.subscribe');
+
+Route::get('/category/{category:slug}', [PostController::class, 'categoryPost']);
+Route::get('/tags/{tag:slug}', [PostController::class, 'tagPost']);
+Route::post('/posts/{post:slug}/comment', [CommentController::class, 'comment']);
